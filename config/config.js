05aa713fe405d32b969504b13d1f243892948f3b
Kerry Archibald <kerryannabell@gmail.com>
2021-10-09 12:22:21 +0200

    add collection and config

--- config/config.js
+++ config/config.js
@@ -2,18 +2,18 @@ const config = {
     app_name: 'Cool Rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
-    collection_contract_address: '0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91',
-    collection_name: '10k One Day Punks',
-    collection_description: '10k "One Day I\'ll Be A Punk"-punks – a homage to the one and only CryptoPunks. Holding a OneDayPunk will give you early access to PunkScapes and reserve a profile on the PunkScape website.',
+    collection_contract_address: '0x868193f5743436b7052549e6a3640580a9355f27',
+    collection_name: 'Rugstore',
+    collection_description: 'we like the rugs.',
     collection_id_from: 0,
     ignore_traits: ['date'], 
     sqlite_file_name: 'database.sqlite',
-    ga: 'G-BW69Z04YTP',
+    ga: undefined,
     main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
     item_path_name: 'punk',
     page_item_num: 60,
     content_image_is_video: false,
-    content_image_frame: 'circle', // circle, rectangle
+    content_image_frame: 'rectangle', // circle, rectangle
     use_wallet: false
 };
 
