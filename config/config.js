7fb47c80a66e32c5cbe08745a1243dd69d6ca491
Kerry Archibald <kerryannabell@gmail.com>
2021-10-09 14:02:49 +0200

    shitty styles

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    app_name: 'Cool Rarity',
+    app_name: 'rugstore rarity',
     app_description: 'Cool Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
     collection_file_name: 'collection.json',
     collection_contract_address: '0x868193f5743436b7052549e6a3640580a9355f27',
@@ -10,7 +10,7 @@ const config = {
     sqlite_file_name: 'database.sqlite',
     ga: undefined,
     main_og_image: 'https://onedaypunk-rarity-tool.herokuapp.com/images/og.png',
-    item_path_name: 'punk',
+    item_path_name: 'rug',
     page_item_num: 60,
     content_image_is_video: false,
     content_image_frame: 'rectangle', // circle, rectangle
